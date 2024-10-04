53549bd20ee0aeb2b45070f401e68d68a7260e61
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-10-03 18:52:30 -0600

    Forms V1.1

--- docs/Practice7/practica7.js
+++ docs/Practice7/practica7.js
@@ -13,30 +13,52 @@ const $errorsMessages = d.querySelectorAll(".error");
 
 // Función de Validación del Formulario
 function validateForm(e) {
+    // Reiniciar mensajes de error y éxito
+    $errorsMessages.forEach((el) => {
+        el.innerText = "";
+    });
+    $successMessage.innerText = "";
+
     let isValid = true;
 
     //Validar Nombre
+    const namePattern = /^[a-zA-Z\s]+$/;
     if ($nameInput.value.trim() === "") {
         $nameError.innerText = "El nombre es obligatorio";
+        isValid = false;
+    } else if (!namePattern.test($nameInput.value.trim())) {
+        $nameError.innerText = "El formato del nombe es incorrecto";
         isValid = false;
     }
 
     //Validar Email
+    let emailPattern = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
     if ($emailInput.value.trim() === "") {
         $emailError.innerText = "El email es obligatorio";
+        isValid = false;
+    } else if (!emailPattern.test($emailInput.value.trim())) {
+        $emailError.innerText = "El formato del correo no es válido";
         isValid = false;
     }
 
     //Validar Password
+    let passwordPattern = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
     if ($passwordInput.value.trim() === "") {
         $passwordError.innerText = "La contraseña es obligatorio";
+        isValid = false;
+    } else if (!passwordPattern.test($passwordInput.value.trim())) {
+        $passwordError.innerText = "La contraseña debe de llevar al menos 1 minúscula, 1 mayúscula, 1 número y 1 carácter especial";
         isValid = false;
     }
 
     //Validar Confirmar Password
+    if ($confirmPasswordInput.value.trim() !== $passwordInput.value.trim()) {
+        $confirmPasswordError.innerText = "Las contraseñas no coinciden";
+        isValid = false;
+    }
 
-    //Prevenir el envío del formulario si hay errores
     if (!isValid) {
+        //Prevenir el envío del formulario si hay errores
         e.preventDefault();
     } else {
         e.preventDefault();
